af192d13c78b143942fe8522b55bdca5c1cf8eff
Ayşe Yılmaz <ayseeyyilmaz96@gmail.com>
2024-05-30 18:54:00 +0300

    design singly linked list

---
+++ Design Singly Linked List/main.py
@@ -0,0 +1,99 @@
+"""
+Design a Singly Linked List class.
+
+Your LinkedList class should support the following operations:
+
+LinkedList() will initialize an empty linked list.
+int get(int i) will return the value of the ith node (0-indexed). If the index is out of bounds, return -1.
+void insertHead(int val) will insert a node with val at the head of the list.
+void insertTail(int val) will insert a node with val at the tail of the list.
+int remove(int i) will remove the ith node (0-indexed). If the index is out of bounds, return false, otherwise return true.
+int[] getValues() return an array of all the values in the linked list, ordered from head to tail.
+Example 1:
+
+Input: 
+["insertHead", 1, "insertTail", 2, "insertHead", 0, "remove", 1, "getValues"]
+
+Output:
+[null, null, null, true, [0, 2]]
+Example 2:
+
+Input:
+["insertHead", 1, "insertHead", 2, "get", 5]
+
+Output:
+[null, null, -1]
+Note:
+
+The index int i provided to get(int i) and remove(int i) is guranteed to be greater than or equal to 0.
+"""
+
+# Singly Linked List Node
+class ListNode:
+    def __init__(self, val, next_node=None):
+        self.val = val
+        self.next = next_node
+
+# Implementation for Singly Linked List
+class LinkedList:
+    def __init__(self):
+        # Init the list with a 'dummy' node which makes 
+        # removing a node from the beginning of list easier.
+        self.head = ListNode(-1)
+        self.tail = self.head
+    
+    def get(self, index: int) -> int:
+        curr = self.head.next
+        i = 0
+        while curr:
+            if i == index:
+                return curr.val
+            i += 1
+            curr = curr.next
+        return -1  # Index out of bounds or list is empty
+
+    def insertHead(self, val: int) -> None:
+        new_node = ListNode(val)
+        new_node.next = self.head.next
+        self.head.next = new_node
+        if not new_node.next:  # If list was empty before insertion
+            self.tail = new_node
+
+    def insertTail(self, val: int) -> None:
+        self.tail.next = ListNode(val)
+        self.tail = self.tail.next
+
+    def remove(self, index: int) -> bool:
+        i = 0
+        curr = self.head
+        while i < index and curr:
+            i += 1
+            curr = curr.next
+        
+        # Remove the node ahead of curr
+        if curr and curr.next:
+            if curr.next == self.tail:
+                self.tail = curr
+            curr.next = curr.next.next
+            return True
+        return False
+
+    def getValues(self) -> list[int]:
+        curr = self.head.next
+        res = []
+        while curr:
+            res.append(curr.val)
+            curr = curr.next
+        return res
+linked_list = LinkedList()
+linked_list.insertHead(1)
+linked_list.insertTail(2)
+linked_list.insertTail(3)
+values = linked_list.getValues()
+print(values) 
+value_at_index = linked_list.get(1)
+print(value_at_index)  
+removed = linked_list.remove(1) 
+print(removed)  
+values = linked_list.getValues()
+print(values)  
